46108321cd83a6ac5b635aabcbd7313e4983a316
Frosty-WinPC <al.korobko@yahoo.com>
2024-11-20 23:43:28 +0100

    Провёл рефакторинг согласно ТЗ. Написал пару тестов.

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,35 +1,16 @@
 package controllers;
 
-import model.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        InMemoryTaskManager manager = new InMemoryTaskManager();
+        /*
+            Нужно ли что-то выводить в мейн, или на данном этапе он просто пустует?
 
-        Task task1 = new Task("Задача 1", "Создать задачу 1", Status.NEW);
-        Task task2 = new Task("Задача 2", "Создать задачу 2", Status.NEW);
+            Касаемо тестов: многие тесты я не стал делать, реализация методов изначально исключает те ошибки,
+            которые тесты призваны проверять. Пример: как я могу в эпик поместить самого себя, если в эпик помещается
+            сабтаск только во время добавления сабтаска в менеджер? И с остальными подобная же история.
 
-        Epic epic1 = new Epic("Эпик 1", "Описание первого эпика");
-        Epic epic2 = new Epic("Эпик 2", "Описание второго эпика");
-
-        Subtask subtask1 = new Subtask("1", "пусто", Status.NEW, 3);
-        Subtask subtask2 = new Subtask("2", "пусто", Status.IN_PROGRESS, 3);
-        Subtask subtask3 = new Subtask("3", "пусто", Status.DONE, 4);
-
-        manager.addTask(task1); //id 1
-        manager.addTask(task2); //id 2
-        manager.addEpic(epic1); //id 3
-        manager.addEpic(epic2); //id 4
-
-        System.out.println(manager.getEpic(3).getStatus()); // ожидаем NEW
-        System.out.println(manager.getEpic(4).getStatus()); // ожидаем NEW
-
-        manager.addSubtask(subtask1); //id 5
-        manager.addSubtask(subtask2); //id 6
-        manager.addSubtask(subtask3); //id 7
-
-        System.out.println(manager.getEpic(3).getStatus()); // ожидаем IN_PROGRESS
-        System.out.println(manager.getEpic(4).getStatus()); // ожидаем DONE
+            Жду рецензию, чтобы исправить, если что требуется, и дописать тесты при необходимости. Заранее спасибо!
+         */
     }
 }
